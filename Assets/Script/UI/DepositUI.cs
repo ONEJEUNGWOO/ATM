f08b0123c5d16114060c14f955f67495975b0e08
원정우 <rjstn155@naver.com>
2025-08-27 09:35:40 +0900

    [Feat] 인풋 필드를 통해 값 입력 및 값을 통한 입금 시스템 구현/ 인풋필드 텍스트 초기화 시스템도 우선 구현

--- Assets/Script/UI/DepositUI.cs
+++ Assets/Script/UI/DepositUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,15 +10,20 @@ public class DepositUI : MonoBehaviour
     [SerializeField] private Button money_1_Button;
     [SerializeField] private Button money_3_Button;
     [SerializeField] private Button money_5_Button;
+    [SerializeField] private Button enterButton;
+    [SerializeField] private TMP_InputField inputField;
 
     public Button BackButton { get { return backButton; } set { backButton = value; } }
     public Button Money_1_Button { get { return money_1_Button; } set { money_1_Button = value; } }
     public Button Money_3_Button { get { return money_3_Button; } set { money_3_Button = value; } }
     public Button Money_5_Button { get { return money_5_Button; } set { money_5_Button = value; } }
+    public Button EnterButton { get { return enterButton; } set { enterButton = value; } }
+    public TMP_InputField InputField { get { return inputField; } set { inputField = value; } }
     
     private void Start()
     {
         BackButton.onClick.AddListener(BackToMainUI);
+        EnterButton.onClick.AddListener(EnterButtonClickTOAddMoney);
     }
 
     public void BackToMainUI()
@@ -28,5 +34,16 @@ public class DepositUI : MonoBehaviour
     public void AddMoney(int cash)  //인스펙터에서 연결 하는 사진이 있어서 그대로 했습니다.
     {
         UIManager.Instance.AddMoney(cash);
+    }
+
+    public void EnterButtonClickTOAddMoney()
+    {
+        if (int.TryParse(InputField.text, out int cash))
+        {
+            if (cash < 0) return;
+
+            AddMoney(cash);
+            //InputField.text = ""; 강의에서는 따로 초기화 되는 부분이 없어서 우선 구현만 해 두었습니다.
+        }
     }
 }
